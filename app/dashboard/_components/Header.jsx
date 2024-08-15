e8383bd23e6cc7500896d08d9e098f45157b13c6
Vedant Pople <vedantpople4@gmail.com>
2024-08-14 19:25:13 -0500

    Final update

--- app/dashboard/_components/Header.jsx
+++ app/dashboard/_components/Header.jsx
@@ -6,23 +6,24 @@ import { usePathname } from 'next/navigation'
 
 function Header() {
 
-    const path = usePathname();
-    useEffect(()=>{
-        //console.log(path)
-    }, [])
+  const path = usePathname();
+  useEffect(() => {
+    //console.log(path)
+  }, [])
 
   return (
     <div className='flex p-4 items-center justify-between bg-secondary shadow-sm'>
-       <Image src={'/logo.svg'} width={160} height={100} alt='logo'/> 
-       {/* <ul className='hidden md:flex gap-6'>
+      {/* //<Image src={'/logo.svg'} width={160} height={100} alt='logo'/>  */}
+      <h2 className='font-bold text-2xl text-primary'>Mock AI</h2>
+      {/* <ul className='hidden md:flex gap-6'>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard' && 'text-primary font-bold'}`}>Dashboard</li>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/questions' && 'text-primary font-bold'}`}>Questions</li>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/upgrade' && 'text-primary font-bold'}`}>Upgrades</li>
         <li className={`hover:text-primary hover:font-bold transition-all cursor-pointer ${path=='/dashboard/how' && 'text-primary font-bold'}`}>How it works?</li>
        </ul> */}
-       <UserButton />
+      <UserButton />
     </div>
-   
+
   )
 }
 
