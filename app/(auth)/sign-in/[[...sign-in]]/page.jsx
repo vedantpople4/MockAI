5d5e9eb522cf6283df3685b98328b7fc90fdcdc9
Vedant Pople <vedantpople4@gmail.com>
2024-08-14 20:53:10 -0500

    SignIn page updated

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -61,12 +61,9 @@ export default function Page() {
                             </a>
 
                             <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-                                Welcome to Squid 🦑
                             </h1>
 
                             <p className="mt-4 leading-relaxed text-gray-500">
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
-                                quibusdam aperiam voluptatum.
                             </p>
                         </div>
 
